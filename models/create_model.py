1cfb60ffd32aa021d97a328e1f16f12751f46229
Noah Kasmanoff <nkasmanoff@gmail.com>
2018-11-09 09:35:01 -0500

    ability to test games today, along with running output document.

---
+++ models/create_model.py
@@ -0,0 +1,188 @@
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../splits_optimizer/model_ready_data.csv')
+
+
+outcome = df['PLUS_MINUS'] #df['home_SPREAD'] + df['PLUS_MINUS'] 
+y = []
+for val in outcome:
+    if val>0: 
+        y.append(1) #home team wins. 
+    else:
+        y.append(0)
+        
+del df['PLUS_MINUS']
+
+
+X = df
+del X['Unnamed: 0']
+del X['Home Team Won?']  #This was the problem from earlier :) 
+del X['road_SPREAD']
+
+
+#ready for data normalization and splitting
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+
+#Split all into training and testing
+X_train,X_test,y_train,y_test = train_test_split(X,y,shuffle=False,test_size = .1)
+
+#prior to normalization, I want to retrieve a list of all of the spreads of the testing data. 
+#This will be used further down the pipeline for converting to moneyline odds. 
+test_spreads = X_test['home_SPREAD']
+
+#del X_test['road_SPREAD'],X_test['home_SPREAD']
+#del X_train['road_SPREAD'],X_train['home_SPREAD']
+
+scaler = MinMaxScaler()
+#Normalization of dataset. 
+scaler.fit(X_train.values)
+X_train = scaler.transform(X_train.values)
+X_test = scaler.transform(X_test.values)
+
+
+#Here's a vanilla neural network courtesy of Sklearn. Let's see how well it does. 
+#I can always tune this model, or apply other ones. 
+
+
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.cross_validation import cross_val_score
+model = MLPClassifier()
+
+model.fit(X_train,y_train)
+
+
+cvs = cross_val_score(model,X_test,y_test,cv=10)
+
+print("Model Accuracy: ", 100*np.mean(cvs), "% +/- " , 100 * 2*np.std(cvs), "%. ")
+
+def spread2ML(spread):
+    """Converts spread into a moneyline value using the equation I calculated. 
+    """
+    if spread <=1.5:
+        
+        ML = 1.71409498 * spread**3 + 10.90008433 * spread **2 + 22.40247106 * spread - 138.20112341
+    else: 
+
+        ML = 1.66494668 * spread**3 -20.03302374 * spread**2 + 101.20347437 * spread - 34.68833849
+    
+    return ML
+
+
+def ML2Payout(ML,bet,win=True):
+    """Convert Moneyline odds to a payout. 
+    """
+    if win:
+        if ML < 0: # - moneyline, 
+        # PAYOUT = BET AMOUNT / (-1 *MONEYLINE ODDS / 100)
+
+            payout = bet / (-1*ML/100)
+
+        elif ML > 0:   #now for the underdog
+        #PAYOUT = BET AMOUNT * ODDS / 100
+            payout = (bet * ML) / 100
+
+            
+        else:
+            payout = bet
+    else:
+        if ML > 0: 
+            payout = -bet
+        elif ML < 0:
+            #in the circumstances where its a favorite, the computer makes you put down more. ie -190 means 19 to win 10. 
+            payout = -bet
+            
+        else:
+            payout = -bet
+    
+    return payout 
+
+def risk2payout(ML,bet,win=True):
+    """Depending on the moneyline, the risk reward formula changes. 
+    """
+    
+    if ML < 0: # if betting on a favorite. 
+        
+        risk = -ML/bet
+        reward = bet
+        if win:
+            payout = reward
+        else:
+            payout = -risk
+        
+    if ML > 0: #if betting on an underdog. 
+        risk = bet
+        reward = ML/bet
+        
+        if win:
+            payout = reward #this is your risked money back, plus the reward. 
+        else:
+            payout = -risk  #this is how much you risked, and it's gone. 
+    
+    return risk, payout
+        
+        
+predictions = model.predict(X_test) #predicted winners of all the test data. 
+
+money_made = 0
+acc_count = 0
+total_winings = 0
+total_losings = 0
+init_bet = 10
+
+for i,prediction in enumerate(predictions):
+       
+     
+    
+    spread = test_spreads.values[i]
+    ML_odds = spread2ML(spread)
+ #   print()
+ #   print("Approx. Odds of Game: ", ML_odds)
+
+   # ML * 
+
+    if y_test[i] == prediction:
+        acc_count+=1
+        risk , winnings = risk2payout(ML_odds,init_bet,win=True)
+     #   print("Correct! Win $", winnings)
+     #   print("You risked $",risk)
+       # print('$',winnings)
+        
+        money_made += winnings
+        total_winings += winnings
+       # if ML_odds < -
+    else:
+        _ , losings = risk2payout(ML_odds,init_bet,win=False)
+     #   print("Wrong! Lose $", -losings)
+
+        money_made += losings
+        total_losings += losings
+
+
+print("# of games selected: ", len(predictions))
+print("Total money made: $", money_made)
+print("Numbers of games chosen correctly: " ,acc_count)
+
+
+
+import pickle
+
+print("exporting tools...")
+
+model_filename = 'finalized_model.sav'
+print("model exported as ", model_filename)
+pickle.dump(model, open(model_filename, 'wb'))
+
+
+scaler_filename = 'finalized_scaler.sav'
+print("scaler exported as ", scaler_filename)
+pickle.dump(scaler, open(scaler_filename, 'wb'))
+
+        
+        
---
+++ todays_games/predict_today.py
@@ -0,0 +1,261 @@
+from datetime import datetime
+from nba_py import Scoreboard
+import numpy as np
+import pandas as pd
+
+today = datetime.now()
+day = today.day
+month = today.month
+year = today.year
+f = open("output.txt", "a")
+f.write('\n')
+
+f.write(str(month)+'/'+str(day)+'/'+str(year)+'\n')
+f.write("----------"+'\n')
+f.close()
+def get_todays_games():
+    
+    today = datetime.now()
+    day = today.day
+    month = today.month
+    year = today.year
+ 
+    ex = Scoreboard(month=month,day=day,year=year)
+    games = ex.game_header()['GAMECODE']
+    matchups = []
+    matchups.append(['road','home'])
+    for game in games:
+        away_team = game[-6:-3]
+        home_team = game[-3:]
+
+        if away_team == 'BKN':
+            away_team = 'BRK'
+        if home_team == 'BKN':
+            home_team = 'BRK'
+        
+        matchups.append([away_team,home_team])
+        
+    return matchups
+
+
+def create_todays_dataset_p1():
+    """Creates a dataset identical to the one used for the ML modeling. This is done by scraping the ngames averages
+    of the teams just listed, along with the spread, and cominbing. 
+    """
+    
+    
+    """Find teams. 
+    """
+    today = datetime.now()
+    day = today.day
+    month = today.month
+    year = today.year
+    
+    
+    matchups = get_todays_games()[1:]
+    
+    #matchups
+    import numpy as np
+    from nba_py import team
+    teams = team.TeamList()
+    teamids = teams.info()
+    teamids = teamids[:-15]
+    teamids = teamids[['TEAM_ID','ABBREVIATION']]
+    teamids = teamids.rename(index=str, columns={"ABBREVIATION": "Team"})
+    teamids = teamids.replace('BKN','BRK')
+    teamids = teamids.sort_values('Team')
+
+    todays_dataframe = []
+    for matchup in matchups:
+        game_array = []
+        for team_ in matchup:
+            TEAM_ID = teamids.loc[teamids['Team'] == team_].values[0,0]
+            #print(team_,TEAM_ID)
+        
+            TEAM_splits = team.TeamLastNGamesSplits(team_id=TEAM_ID,season='2018-19')
+       # print(TEAM_splits.last20())
+            df = TEAM_splits.last20()
+        
+            #retain (and create) the columns already proven to be statistically correlated to outcome. 
+            df['AST/TO'] = df['AST']/df['TOV']
+
+            df = df[['FGM','FG3M','FTM','DREB','AST','STL',
+                     'TOV','BLK','PTS','AST/TO','FG3_PCT',
+                     'FG_PCT','FT_PCT']]
+           # df['AST/TO'] = df['AST']/df['TOV']
+            game_array.append(df.values)
+        
+        matchup_array = np.concatenate((game_array[0],game_array[1]),axis=1)
+        todays_dataframe.append(matchup_array)
+
+    #quick formating!
+    todays_dataframe = np.array(todays_dataframe)
+    todays_dataframe = todays_dataframe[:,0,:]
+    return todays_dataframe
+
+def scrape_espn_for_today_spreads(todays_dataframe):
+    """Scrape and clean from ESPN, which has the info of all NBA games today and their lines. 
+    """
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    import urllib
+    matchups = get_todays_games()[1:]
+
+
+    url  = "http://www.espn.com/nba/lines/_/date"
+    page = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(page, "lxml")
+    tables = soup.find_all('table')
+    table_df = pd.DataFrame([])
+    for table in tables:
+        table_df =table_df.append(pd.read_html(str(table)))
+
+    team_that_plays = []
+    spread_of_that_team = []
+    for i, row in enumerate(table_df[1]):
+        if row == 'SPREAD':
+           # print(row)
+            try:
+           #     print(table_df[1][i+1])
+        
+                eh = table_df[1][i+1]
+            except: 
+                eh = table_df[1][i+2]
+          #      print(eh)
+
+            sign = eh[0]
+            try:
+                spread, _ = eh[1:6].split('-')
+            except:
+                spread, _ = eh[1:6].split('+')
+
+       # spread, _ = eh[0:5].split('-')
+       # print(spread)
+           # print(sign + spread)
+            for i, char in enumerate(eh): 
+                team = eh[i:i+3]
+
+                if char.isalpha():
+                    team = eh[i:i+3]
+                    if team[-1].isalpha() == False:
+                        if team == 'SA:':
+                            team = 'SAS'
+                        if team == 'NY:':
+                            team = 'NYK'
+                        if team == 'GS:':
+                            team = 'GSW'
+                    #print(team)
+
+                    break
+    
+              #  if eh[i+3].isalpha() == False:
+              #      print(team)
+              #      print("BAD!!")
+               # team = eh[i:i+3]
+             ##   if team =='SA':
+             #       team = 'SAS'
+
+               # break
+                
+        
+        
+       # print("Spread of the game? " , team, sign+spread)
+            team_that_plays.append(team)
+            spread_of_that_team.append(sign+spread)
+            
+    home_spread = []
+   # print("TEAM THAT PLAYS:")
+   # print(team_that_plays)
+    for s,team in enumerate(team_that_plays):
+      #  print(team)
+        for game in matchups:
+            try:
+                ind = game.index(team)
+                if ind == 0:
+                  #  print(team + " is on the road")
+                 #   print("home spread is therefore opposite of ",float(spread_of_that_team[s]))
+                 #  print("home spread is therefore ",-float(spread_of_that_team[s]))
+                    home_spread.append(-float(spread_of_that_team[s]))
+
+                else:
+                 #   print(team + " is at home")
+                    home_spread.append(-float(spread_of_that_team[s]))
+    
+
+            except:
+                pass
+        
+    ready_for_it = []
+    for i in range(len(todays_dataframe)):
+        ready = list(todays_dataframe[i])
+        ready.append(home_spread[i])
+        ready_for_it.append(ready)
+  # todays_dataframe[i] = list(todays_dataframe[i]).append(home_spread[i])
+
+    ready_for_it = np.array(ready_for_it)
+    
+    return matchups,ready_for_it
+
+
+
+todays_splits = create_todays_dataset_p1()
+
+
+matchups, data = scrape_espn_for_today_spreads(todays_splits)
+
+
+import pickle 
+
+#print("Loading in models...")
+
+model_path = '../models/finalized_model.sav'
+# load the model from disk
+loaded_model = pickle.load(open(model_path, 'rb'))
+#result = loaded_model.score(X_test, Y_test)
+#print(result)
+
+scaler_path = '../models/finalized_scaler.sav'
+loaded_scaler = pickle.load(open(scaler_path, 'rb'))
+
+X_today = data
+
+
+X_today = loaded_scaler.transform(X_today)
+
+for i,game in enumerate(X_today):
+    prediction = loaded_model.predict([game])[0]
+    
+    print("Game: ", matchups[i])
+    print("Winner: ",matchups[i][prediction])
+
+
+
+
+
+def spread2ML(spread):
+    """Converts spread into a moneyline value using the equation I calculated. 
+    """
+    if spread <=1.5:
+        
+        ML = 1.71409498 * spread**3 + 10.90008433 * spread **2 + 22.40247106 * spread - 138.20112341
+    else: 
+
+        ML = 1.66494668 * spread**3 -20.03302374 * spread**2 + 101.20347437 * spread - 34.68833849
+    
+    return ML
+
+
+f = open("output.txt", "a")
+spreads = data[:,-1]
+for i,game in enumerate(X_today):
+    prediction = loaded_model.predict([game])[0]
+    
+
+    f.write("Game: " + str(matchups[i][0]) + ' @ ' + str(matchups[i][1])+'\n')
+    f.write("Predicted Winner: " + str(matchups[i][prediction])+'\n')
+    f.write("Home Team Spread: " + str(matchups[i][1]) + str(spreads[i])+'\n')
+    f.write("Approximate Moneyline Odds: " + str(matchups[i][0])+str(spread2ML(-spreads[i]))+" "+str(matchups[i][1])+str(spread2ML(spreads[i]))+'\n')
+    #f.write()
+
+f.close()
+
